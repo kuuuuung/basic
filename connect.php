51279577efa65d70072299b095c75977f6369ecf
kungnang <s6102041520025@email.kmutnb.ac.th>
2021-05-17 18:58:18 +0700

    changed code

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+//Create DB
+$con= new mysqli($servername,$username,$password);
+
+//Check
+if($con->connect_error){
+    die("connect failed".$con->connect_error);
+}
+echo "succes";
+*/
+
+?>
